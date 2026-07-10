74489c272c95693f1934e71971b19b1358ebdd0a
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-07-10 23:21:31 +0700

    chore: update compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,17 +5,12 @@ plugins {
 
 android {
     namespace = "dev.stefano.enuventory"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "dev.stefano.enuventory"
         minSdk = 24
-        //noinspection OldTargetApi
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
