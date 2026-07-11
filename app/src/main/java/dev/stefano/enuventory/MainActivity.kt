c8bda48560bbe2d9a3b0182af1f77eeabba6181d
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-07-11 21:44:21 +0700

    feat: enable edge-to-edge in MainActivity

--- app/src/main/java/dev/stefano/enuventory/MainActivity.kt
+++ app/src/main/java/dev/stefano/enuventory/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,6 +16,7 @@ import dev.stefano.enuventory.ui.theme.EnuTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             EnuTheme {
                 var pageState by remember { mutableStateOf(HomeUserState.Normal) }
