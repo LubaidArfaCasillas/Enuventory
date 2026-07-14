35e700185bb94290824f15cbabacb66c03dbd48e
Stefano Nirwana <stefano.budi96@smk.belajar.id>
2026-07-14 11:11:30 +0700

    fix: issue #7

--- app/src/main/java/dev/stefano/enuventory/ui/screen/auth/LoginScreen.kt
+++ app/src/main/java/dev/stefano/enuventory/ui/screen/auth/LoginScreen.kt
@@ -9,11 +9,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -61,7 +61,8 @@ fun LoginScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .background(EnuTheme.colors.surfaceDefaultBase),
+                .background(EnuTheme.colors.surfaceDefaultBase)
+                .imePadding(),
             contentAlignment = Alignment.Center
         ) {
             Column(
